c0fdafcfbdc5bc5a5653a969f42df0e4f176aa06
한성수 <sshan@lgupluspartners.co.kr>
2025-03-04 17:54:47 +0900

    supabase connecting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 import Image from "next/image";
 import hamm from "@/resources/images/hammm.jpg";
+import { supabase } from "@/lib/supabaseClient";
+import { PostgrestError } from "@supabase/supabase-js";
 
-export default function Home() {
+type Board = {
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+};
+
+export default async function Home() {
+  const {
+    data: boards,
+    error,
+  }: { data: Board[] | null; error: PostgrestError | null } = await supabase
+    .from("board")
+    .select("*")
+    .order("created_at", { ascending: false });
+
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>모두의 (투명한) 돼지저금통</h1>
@@ -14,6 +31,28 @@ export default function Home() {
         height={500}
         alt="모두의 (투명한) 돼지저금통"
       ></Image>
+      <div
+        style={{
+          margin: "50px auto 0",
+        }}
+      >
+        <h2>Data 출력 테스트</h2>
+        {error && (
+          <p style={{ color: "red" }}>
+            게시글을 불러오는 중 오류가 발생했습니다.
+          </p>
+        )}
+        {!error && boards && (
+          <ul>
+            {boards.map((board) => (
+              <li key={board.id}>
+                <h2>{board.title}</h2>
+                <p>{board.content}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error("Supabase URL이 설정되지 않았습니다.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
